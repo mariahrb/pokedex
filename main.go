8284505597832415dc9677ac9d11151b842ede20
mariahrb <mariahrangelbarreto@gmail.com>
2025-08-27 09:04:18 -0400

    feat: implement CLI commands and input cleaning functionality

--- main.go
+++ main.go
@@ -1,9 +1,92 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
+	for {
+		fmt.Printf("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		text := scanner.Text()
+
+		words, err := cleanInput(text)
+		if err != nil {
+			fmt.Printf("Error cleaning input: %v\n", err)
+			continue
+		}
+
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		cmd, ok := commands[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		if err := cmd.callback(); err != nil {
+			fmt.Println("Error:", err)
+		}
+	}
+}
+
+func cleanInput(text string) ([]string, error) {
+
+	text = strings.TrimSpace(text)
+	text = strings.ReplaceAll(text, ",", " ")
+	parts := strings.Fields(text)
+
+	for i := range parts {
+		parts[i] = strings.ToLower(parts[i])
+	}
+
+	return parts, nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage; \n")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
